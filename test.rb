49c66f077374f16325fc3fa8c7daebb8d66c2391
prakashdwd <39290658+prakashdwd@users.noreply.github.com>
2018-05-15 17:00:06 +0530

    Add files via upload
    first commit to the master branch

---
+++ test.rb
@@ -0,0 +1,25 @@
+$num = 0
+while true
+   c=-1;
+   str = String.new("")
+   str=gets
+   
+   if str.include? "add"
+		
+   end
+   if str.include? "sub"
+		
+   end
+   if str.include? "mul"
+		
+   end
+   if str.include? "div"
+		
+   end
+   if str.include? "cancel"
+		
+   end
+   if str.include? "exit"
+		
+   end
+end
