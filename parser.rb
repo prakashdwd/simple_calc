f18a7f7b32831a8e8b791685930657389c7e5608
prakashdwd <prakash.d.aux@go-jek.com>
2018-05-16 22:53:46 +0530

    More corner cases handled!

--- parser.rb
+++ parser.rb
@@ -2,14 +2,23 @@ class Parser
    def initialize(arg)
       @input=arg
    end
+
+   def letters?(arg)
+     arg.match(/[A-Za-z]/) ? true : false
+   end
+   
    def parse
-		operation = @input.split().first.chomp
-      number = @input.split().last.chomp
-		value = number.to_f
-		wordCount = @input.split(/\s+/).length
-      check = value.to_s.chomp
+      operation = @input.split().first
+      number = @input.split().last
+      value = number.to_f
+      wordCount = @input.split(/\s+/).length
+      check = value.to_s
 
       if wordCount > 2
+        return nil
+      end
+
+      if wordCount == 2 and letters?(number)
 			return nil
       else
          return operation , value
